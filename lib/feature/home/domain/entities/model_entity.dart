77b3f02959b2fa41bea0e312943521c9f95e9acc
AQ <cshazemelwazery@gmail.com>
2024-08-19 14:21:14 +0300

    add entities in  project

---
+++ lib/feature/home/domain/entities/model_entity.dart
@@ -0,0 +1,8 @@
+class ModelEntity{
+  String title;
+  num price;
+  String description;
+  List<String>image;
+
+  ModelEntity({required this.title, required this.price, required this.description, required this.image});
+}
